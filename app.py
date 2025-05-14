11b05d8b03700b1a3b2143bc4a507a9445e3361f
Lenoir Pedro <pedrolenoir20@gmail.com>
2025-05-14 00:05:39 -0300

    Adiciona rota / para mensagem de boas-vindas

--- app.py
+++ app.py
@@ -30,3 +30,8 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
 
+@app.route("/")
+def index():
+    return "API de Cotação Online! Use /cotacao?moeda=USD"
+
+
